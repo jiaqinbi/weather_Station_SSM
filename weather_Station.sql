765e318ff3a6d17df8775c96e8ff16685c8b40e2
unknown <jiaqinbi>
2019-10-03 14:46:12 +0800

    second commit

---
+++ weather_Station.sql
@@ -0,0 +1,56 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 5.1.55-community : Database - weather_station_sql
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`weather_station_sql` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `weather_station_sql`;
+
+/*Table structure for table `datalist` */
+
+DROP TABLE IF EXISTS `datalist`;
+
+CREATE TABLE `datalist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `temperature` varchar(30) DEFAULT NULL,
+  `humidity` varchar(30) DEFAULT NULL,
+  `air_pressure` varchar(30) DEFAULT NULL,
+  `wind_direction` varchar(30) DEFAULT NULL,
+  `wind_speed` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `datalist` */
+
+insert  into `datalist`(`id`,`username`,`temperature`,`humidity`,`air_pressure`,`wind_direction`,`wind_speed`) values (1,'admin','35','30','90','70','45');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`) values (1,'admin','123456'),(2,'admin1','123456');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
